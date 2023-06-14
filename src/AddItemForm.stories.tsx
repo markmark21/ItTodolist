ac19740aba8959db3decd9530d872d170b921132
Mark Khramov <hramov.mark321@gmail.com>
2023-06-14 19:16:28 +0300

    addItemForm story

--- src/AddItemForm.stories.tsx
+++ src/AddItemForm.stories.tsx
@@ -1,18 +1,21 @@
 import {AddItemForm} from "./AddItemForm";
 import {Meta, StoryObj} from "@storybook/react";
+import {action} from '@storybook/addon-actions'
 
 
 const meta: Meta<typeof AddItemForm> = {
     title: 'AddItemForm',
-    component: AddItemForm
+    component: AddItemForm,
+    tags: ['autodocs'],
 }
 
 export default meta;
 
 type Story = StoryObj<typeof AddItemForm>;
 
+const callBack = action('Button "add" was pressed')
 export const AddItemFormExample: Story = {
     args: {
-        addItem: (title) => {alert(title)}
+        addItem: (callBack)
     }
 }
