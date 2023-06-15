556d46e3ce2119756f41d74c3bbd569153012bff
Mark Khramov <hramov.mark321@gmail.com>
2023-06-15 17:54:32 +0300

    story for EditableSpan

---
+++ src/EditableSpan.stories.tsx
@@ -0,0 +1,22 @@
+import {EditableSpan} from "./EditableSpan";
+import {Meta, StoryObj} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+
+
+const meta: Meta<typeof EditableSpan> = {
+    title: 'EditableSpan',
+    component: EditableSpan,
+    tags: ['autodocs']
+}
+
+export default meta;
+
+type Story = StoryObj<typeof EditableSpan>;
+
+const changeCallBack = action('Value Changed')
+export const EditableSpanBaseExample: Story = {
+    args: {
+        value: 'Start value',
+        onChange: (changeCallBack)
+    }
+}
