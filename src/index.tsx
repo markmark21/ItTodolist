3c779501db0d54172c66a3e5eacd42ecd1954bf3
Mark Khramov <hramov.mark321@gmail.com>
2023-06-21 23:04:01 +0300

    1

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import AppWithRedux from './AppWithRedux';
+import {store} from './state/store';
+import {Provider} from 'react-redux';
+
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container);
+root.render(
+    <Provider store={store}>
+        <AppWithRedux/>
+    </Provider>
+)
+
+serviceWorker.unregister();
